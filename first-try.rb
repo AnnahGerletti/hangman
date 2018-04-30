23fb927c44dcd1fcc186788ac90bae93d418e14e
Annah <annah.gerletti@fluxfederation.com>
2018-05-01 11:07:47 +1200

    added explaination comments, and a challenge of printing out users guesses

--- first-try.rb
+++ first-try.rb
@@ -1,4 +1,4 @@
-
+SECRET = 'love'.chars #the chars method breaks up string into arr of strings
 
 def ask_player
     answer = gets.chomp
@@ -11,10 +11,18 @@ def ask_player
     end
 end
 
-answer = false 
-while !answer do 
-     puts "Do you want to play Hangman, yes or no"
-    answer = ask_player
-end
 
 # until answer do - does the same thing as the above loop
+
+def run 
+    answer = false 
+    while !answer do 
+         puts "Do you want to play Hangman, yes or no"
+        answer = ask_player
+    end
+end
+
+run #calling the run method, incased the second answer variable 
+
+#need to compare the guess to secret 
+# get some guesses, then print them out added new guess on old printed guess
