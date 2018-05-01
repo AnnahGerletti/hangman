feb605bbea1c5a26846d8bcccb3b385b152282dc
Annah <annah.gerletti@fluxfederation.com>
2018-05-02 10:08:19 +1200

    fixed linting errors

--- first-try.rb
+++ first-try.rb
@@ -1,48 +1,50 @@
-SECRET = 'dog'.chars #the chars method breaks up string into arr of strings
+SECRET = 'dog'.chars # the chars method breaks up string into arr of strings
 
 def ask_player
-    answer = gets.chomp
-    if answer == "yes" 
-        puts "Lets Play!"
-        return true
-    else 
-        puts "whatever"
-        return false
-    end
+  answer = gets.chomp
+  if answer == 'yes'
+    puts 'Lets Play!'
+    return true
+  else
+    puts 'whatever'
+    return false
+  end
 end
-
 
 # until answer do - does the same thing as the above loop
 
-def run 
-    answer = false 
-    while !answer do 
-         puts 'Do you want to play Hangman, yes or no'
-        answer = ask_player
-    end
+def run
+  answer = false
+  until answer
+    puts 'Do you want to play Hangman, yes or no'
+    answer = ask_player
+  end
 end
 
-run # calling the run method, incased the second answer variable 
+run # calling the run method, incased the second answer variable
 
 # need to compare the guess to secret
 # get some guesses, then print them out added new guess on old printed guess
 
 def make_a_guess
-    puts 'please guess a letter'
-    guess = gets.chomp
-    puts 'this your guess - ' + guess
-    puts "this is also your guess #{guess}"
-    return guess
+  puts 'please guess a letter'
+  guess = gets.chomp
+  puts 'this your guess - ' + guess
+  puts "this is also your guess #{guess}"
+  guess
 end
 
 winning = false
 guess_arr = []
 count = 0
 
-while count < 2 do 
-    guess = make_a_guess
-    guess_arr.push(guess)
-    print guess_arr
-    count += 1
+while count < 2
+  guess = make_a_guess
+  guess_arr.push(guess)
+  print guess_arr
+  count += 1
 
 end
+
+#
+# line 42 change the while loop to an until and adding the conditional that count = 2. Stops the loop on line 19. Why is this happening?
