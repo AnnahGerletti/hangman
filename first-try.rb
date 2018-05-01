26d1995749d1a9fe2fc933c21c991a359b15b1c3
Annah <annah.gerletti@fluxfederation.com>
2018-05-01 14:07:25 +1200

    refactored code

--- first-try.rb
+++ first-try.rb
@@ -22,29 +22,26 @@ def run
     end
 end
 
-run #calling the run method, incased the second answer variable 
+run # calling the run method, incased the second answer variable 
 
-#need to compare the guess to secret 
+# need to compare the guess to secret
 # get some guesses, then print them out added new guess on old printed guess
 
 def make_a_guess
 
     puts "please guess a letter"
     guess = gets.chomp
-    puts "this your guess - " + guess
+    puts 'this your guess - ' + guess
     puts "this is also your guess #{guess}"
-    
-    return guess 
+    return guess
 end
 
 winning = false
 guess_arr = []
 
-while !winning do 
-guess = make_a_guess
-
-guess_arr.push(guess)
-print guess_arr
+until winning do 
+    guess = make_a_guess
+    guess_arr.push(guess)
+    print guess_arr
 
 end
-
