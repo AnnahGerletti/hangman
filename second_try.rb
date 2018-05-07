92b59e02437ad4fe25e7ca69d3e0565d9f7a98cc
Annah <annah.gerletti@fluxfederation.com>
2018-05-07 16:25:45 +1200

    Working hangman
    now need to structure better, add some formating for the console. Could add some more game logic.

--- second_try.rb
+++ second_try.rb
@@ -2,14 +2,13 @@
 # more structured
 
 # setup
-lives = 10
+lives = 9
 guess_arr = []
-won = false
 
 SECRET = 'ball'.chars
 
 def dashes(guess_arr)
- SECRET.map do |chr|
+  SECRET.map do |chr|
    if guess_arr.include? chr
     chr
    else
@@ -18,36 +17,37 @@ def dashes(guess_arr)
   end.join ','
 end
 
-def check_win
-  if SECRET.include? '-'
-    won = false
-  else
-    won = true
-    puts 'You are a Winner'
+def game_won?(guess_arr)
+  SECRET.each do |char|
+    if !guess_arr.include? char
+      return false
+    end
   end
+   true
 end
 
 puts 'Normal Hangman rules'
 puts 'You have 10 lives to guess the word'
-puts 'Every Worng guess you will lose a life'
-puts 'Here we go'
+puts 'Every Wrong guess you will lose a life'
+puts "\n"'Here we go'
 
-  while lives >= 0 
-    puts 'guess a letter'
+  until lives <= 0 || game_won?(guess_arr)
+    puts 'Guess a letter:'
     guess = gets.chomp
     guess_arr.push(guess)
-    check_win
-    
     if SECRET.include? guess
       puts 'that was a correct guess'
       print guess_arr
     else
-      puts 'nope, guess again'
-      puts "you have #{lives} lives left"
+      puts 'Nope, guess again:'
+      puts "You have #{lives} lives left"
       lives -= 1
       puts 'this is what you have already guessed'
       puts guess_arr.to_s
     end
-    puts dashes guess_arr
+      puts dashes guess_arr
+     if game_won?(guess_arr) 
+       puts 'Congradulations, You Won the game!'
+     end
   end
 
