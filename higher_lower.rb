b8661fe8db6ff7fe974472843158753514753928
Annah <annah.gerletti@fluxfederation.com>
2018-05-14 14:53:48 +1200

    Obj orientated design
    finished the Higherlower class that includes all the logic of the game. Adding a playing class that works with the player interaction with the game.

--- higher_lower.rb
+++ higher_lower.rb
@@ -9,18 +9,40 @@ class HigherLower
   NUMBERS = Array(1..100)
   INITIAL_LIVES = 6 # constant class var
 
-  def initialize 
+  def initialize
     @random_num = NUMBERS.sample()
     @lives = INITIAL_LIVES # instance level var
+    @won = false
   end
 
-  def status
+  def print_status
     puts "you have #{@lives} lives left"
+    if game_lost?
+      puts "game over. The hidden Number was #{@random_num}" 
+    elsif game_won?
+      puts "winner"
+    end
+  end
+
+  def game_over? 
+    game_lost? || game_won?
+    # you have guess the right number
+  end
+
+  def game_lost?
+     @lives.zero?
+  end
+
+  def game_won?
+    @won
   end
 
   def guess(number)
+    return false if game_over?
+
     if number == @random_num
       puts 'You have gueesed the number, You Win!'
+      @won = true
     elsif number > @random_num
       puts 'too high'
       @lives -= 1
@@ -28,5 +50,25 @@ class HigherLower
       puts 'The guess was to low'
       @lives -= 1
     end
+
+    true
   end
 end
+
+class PlayHighLow
+  def initialize
+    @game = HigherLower.new
+  end
+
+  def play
+    puts 'Welcome to the High Low game!'
+    @game.print_status
+    puts 'Please guess a Number between 1-100'
+    players_guess = gets.chomp
+    @game.guess(players_guess)
+    # until @game.game_over?
+    # end
+  end
+end
+
+PlayHighLow.new.play
