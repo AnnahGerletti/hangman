7f92ecf0c171183ee39f1ace35ff23472d0fa075
Annah <annah.gerletti@fluxfederation.com>
2018-05-14 13:19:58 +1200

    working classes
    applied a game class, that has class based methods and instance methods

--- higher_lower.rb
+++ higher_lower.rb
@@ -1,19 +1,32 @@
-numbers = Array(1..100)
-random_num = numbers.sample()
-
-lives = 6
-
-#game picks a number
-#player enters a guess
-#guess is checked against random num
-#game tells player if number is higher or lower and how many lives they have left
+# game picks a number
+# prompt status of game
+# player enters a guess
+# guess is checked against random num
+# game tells player if number is higher or lower and how many lives they have left
 #
 
-class GuessNum
-  def initialize(guess)
-    @guess = guess
+class HigherLower
+  NUMBERS = Array(1..100)
+  INITIAL_LIVES = 6 # constant class var
+
+  def initialize 
+    @random_num = NUMBERS.sample()
+    @lives = INITIAL_LIVES # instance level var
   end
 
-  def greater_than(guess)
-    if @guess >= 50
+  def status
+    puts "you have #{@lives} lives left"
+  end
 
+  def guess(number)
+    if number == @random_num
+      puts 'You have gueesed the number, You Win!'
+    elsif number > @random_num
+      puts 'too high'
+      @lives -= 1
+    else
+      puts 'The guess was to low'
+      @lives -= 1
+    end
+  end
+end
