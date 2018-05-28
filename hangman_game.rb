5df0eb445492a7c2eda7cb7793838858a7f0e118
Annah <annah.gerletti@fluxfederation.com>
2018-05-28 16:57:45 +1200

    updated some methods
    Now I am passing in the initial lives so when you make a new instance of game you can pass in how many lives you want.

---
+++ hangman_game.rb
@@ -0,0 +1,51 @@
+require_relative './hangman_dictionary.rb'
+
+class Game
+  attr_reader :saved_guess, :initial_lives, :secret
+
+  def initialize(initial_lives: 10)
+   @saved_guess = []
+   @initial_lives = initial_lives
+   @secret = Dictionary.new.secret
+  end
+
+  def lives_left
+    initial_lives - (saved_guess.uniq - secret).size
+  end
+
+  def over?(secret)
+    lost? || won?(secret)
+  end
+
+  def lost?
+    lives_left.zero?
+  end
+
+  def won?(secret)
+    secret.all? do |letter|
+      saved_guess.include?(letter)
+    end
+  end
+
+  def take_turn(guess)
+    saved_guess.push(guess)
+  end
+
+  def correct_guess(added_guess, secret)
+    if secret.include?(added_guess)
+     true
+    else
+     false
+    end
+  end
+
+  def dashes(secret)
+    secret.map do |char|
+      if saved_guess.include?(char)
+        char
+      else
+        '-'
+      end
+    end.join ','
+  end
+end
