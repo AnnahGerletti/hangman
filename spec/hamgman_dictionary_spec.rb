cba7d496da9b69f2e0336d68d50f6ef451c613df
Annah <annah.gerletti@fluxfederation.com>
2018-05-29 16:51:34 +1200

    started writing test for dictionary

---
+++ spec/hamgman_dictionary_spec.rb
@@ -0,0 +1,15 @@
+require_relative = '../hangman_dictionary'
+
+describe '#intialize' do 
+  context 'when a game is initialized' do
+    context 'a random word is sampled' do
+      it 'choses a random word from array' do 
+        dict= Dictionary.new
+
+        
+
+
+      end
+    end 
+  end
+end
