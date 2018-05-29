c0c9646c6e3b5a0ab3d03df90b81afd3250b093d
Annah <annah.gerletti@fluxfederation.com>
2018-05-29 14:45:51 +1200

    major refactor
    removed the connection to the Dictionary, seperation of responsabilities
    removed passing secret to many of the methods, The randomly selected
    Dictionary word is passed to the new instance of the game in hangman_main

--- hangman_game.rb
+++ hangman_game.rb
@@ -1,15 +1,15 @@
-require_relative './hangman_dictionary.rb'
+
 
 class Game
   attr_reader :saved_guess, :initial_lives, :secret
 
-  def initialize(initial_lives: 10)
+  def initialize(secret:, initial_lives: 10 )
    @saved_guess = []
    @initial_lives = initial_lives
-   @secret = Dictionary.new.secret
+   @secret = secret
   end
 
-  def set_secret (new_secret) # override the dictionary secret, with a set secret written in the test.
+  def set_secret(new_secret) # override the dictionary secret, with a set secret written in the test.
     @secret = new_secret
   end
 
@@ -17,15 +17,15 @@ class Game
     initial_lives - (saved_guess.uniq - secret).size
   end
 
-  def over?(secret)
-    lost? || won?(secret)
+  def over?
+    lost? || won?
   end
 
   def lost?
     lives_left.zero?
   end
 
-  def won?(secret)
+  def won?
     secret.all? do |letter|
       saved_guess.include?(letter)
     end
@@ -35,7 +35,7 @@ class Game
     saved_guess.push(guess)
   end
 
-  def correct_guess(added_guess, secret)
+  def correct_guess(added_guess)
     if secret.include?(added_guess)
      true
     else
@@ -43,7 +43,7 @@ class Game
     end
   end
 
-  def dashes(secret)
+  def dashes
     secret.map do |char|
       if saved_guess.include?(char)
         char
